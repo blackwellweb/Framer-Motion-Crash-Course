67188b3cdf45cbdec35efcaeb1f15198f391f7b9
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2020-06-07 15:34:57 +0100

    Hover Animations

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -7,9 +7,9 @@ const Base = ({ addBase, pizza }) => {
 
   return (
     <motion.div className="base container"
-    initial={{x: '100vw'}}
-    animate={{x: 0}}
-    transition={{type: 'spring', delay: 0.5}}
+      initial={{ x: '100vw' }}
+      animate={{ x: 0 }}
+      transition={{ type: 'spring', delay: 0.5 }}
     >
 
       <h3>Step 1: Choose Your Base</h3>
@@ -17,21 +17,30 @@ const Base = ({ addBase, pizza }) => {
         {bases.map(base => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
-            <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
-            </li>
+            <motion.li key={base} onClick={() => addBase(base)}
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
           )
         })}
       </ul>
 
       {pizza.base && (
         <motion.div className="next"
-        initial={{x: '-100vw'}}
-        animate={{x: 0}}
-        transition={{ type: 'spring', stiffness: 120}}
+          initial={{ x: '-100vw' }}
+          animate={{ x: 0 }}
+          transition={{ type: 'spring', stiffness: 120 }}
         >
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button
+              whileHover={{
+                scale: 1.1,
+                textShadow: '0px 0px 8px rgb(255, 255,255)',
+                boxShadow: '0px 0px 8px rgb(255, 255,255)'
+              }}
+            >Next</motion.button>
           </Link>
         </motion.div>
       )}
